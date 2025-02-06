06c93607aec38fa8546c97735a034c867401b901
stemfusion-22 <stemfusion22@gmail.com>
2025-02-06 11:45:43 +0530

    Add files via upload

---
+++ tailwind.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-blue': '#0097b2',
+        },
+      },
+    },
+    plugins: [],
+  }
